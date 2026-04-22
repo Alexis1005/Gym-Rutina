88210f6e065f2bad96b1df879c0722824806351d
Alexis Vespa <alexis_1722@hotmail.es>
2026-04-22 17:00:08 -0300

    Add package declaration and imports in HomeController
    Added package declaration and imports for HomeController.

--- src/main/java/com/joana/gymrutine/controller/HomeController.java
+++ src/main/java/com/joana/gymrutine/controller/HomeController.java
@@ -1,3 +1,8 @@
+package com.joana.gymrutine.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class HomeController {
 
